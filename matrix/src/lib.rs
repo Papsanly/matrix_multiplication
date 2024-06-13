0bf0744ae5b696950b32aa355398393c69e1126a
Papsanly <papsanlysenko@gmail.com>
2024-06-13 21:32:12 +0300

    remove redundant trait bounds for matrix struct

--- matrix/src/lib.rs
+++ matrix/src/lib.rs
@@ -1,19 +1,18 @@
-use num_traits::Num;
 use std::ops::{Index, IndexMut};
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct Matrix<T: Num + Clone> {
+pub struct Matrix<T: Clone> {
     pub rows: usize,
     pub cols: usize,
     buf: Vec<T>,
 }
 
-impl<T: Num + Clone> Matrix<T> {
-    pub fn new(rows: usize, cols: usize) -> Self {
+impl<T: Clone> Matrix<T> {
+    pub fn new(fill: T, rows: usize, cols: usize) -> Self {
         Self {
             rows,
             cols,
-            buf: vec![T::zero(); rows * cols],
+            buf: vec![fill; rows * cols],
         }
     }
 
@@ -27,14 +26,14 @@ impl<T: Num + Clone> Matrix<T> {
     }
 }
 
-impl<T: Num + Clone> Index<usize> for Matrix<T> {
+impl<T: Clone> Index<usize> for Matrix<T> {
     type Output = [T];
     fn index(&self, index: usize) -> &Self::Output {
         &self.buf[index * self.cols..(index + 1) * self.cols]
     }
 }
 
-impl<T: Num + Clone> IndexMut<usize> for Matrix<T> {
+impl<T: Clone> IndexMut<usize> for Matrix<T> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.buf[index * self.cols..(index + 1) * self.cols]
     }
--- sequential/src/lib.rs
+++ sequential/src/lib.rs
@@ -4,7 +4,7 @@ use num_traits::{Num, NumAssign};
 pub fn multiply<T: Num + NumAssign + Clone + Copy>(left: Matrix<T>, right: Matrix<T>) -> Matrix<T> {
     assert_eq!(left.cols, right.rows, "Invalid matrix dimensions");
     assert_eq!(left.rows, right.cols, "Invalid matrix dimensions");
-    let mut res = Matrix::new(left.rows, right.cols);
+    let mut res = Matrix::new(T::zero(), left.rows, right.cols);
     for i in 0..left.rows {
         for j in 0..right.cols {
             let mut sum = T::zero();
