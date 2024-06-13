f495e7cb84887c9521fa6188550d68ca8e11b6d9
Papsanly <papsanlysenko@gmail.com>
2024-06-13 21:54:32 +0300

    remove wrong assertion

--- sequential/src/lib.rs
+++ sequential/src/lib.rs
@@ -3,7 +3,6 @@ use num_traits::{Num, NumAssign};
 
 pub fn multiply<T: Num + NumAssign + Clone + Copy>(left: Matrix<T>, right: Matrix<T>) -> Matrix<T> {
     assert_eq!(left.cols, right.rows, "Invalid matrix dimensions");
-    assert_eq!(left.rows, right.cols, "Invalid matrix dimensions");
     let mut res = Matrix::new(T::zero(), left.rows, right.cols);
     for i in 0..left.rows {
         for j in 0..right.cols {
