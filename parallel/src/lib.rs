d8a6f8c2b5b67cef736a4cc165ccf89a98bbcdcb
Papsanly <papsanlysenko@gmail.com>
2024-06-14 19:54:28 +0300

    improved implementation of parallel to not use Mutex

--- parallel/src/lib.rs
+++ parallel/src/lib.rs
@@ -1,38 +1,38 @@
 use matrix::Matrix;
-use num_traits::{Num, NumAssign};
-use std::{
-    sync::{Arc, Mutex},
-    thread,
-};
+use num_traits::NumAssign;
+use std::{sync::Arc, thread};
 
-pub fn multiply<T: Num + NumAssign + Clone + Copy + Send + Sync + 'static>(
+pub fn multiply<T: NumAssign + Clone + Copy + Send + Sync + 'static>(
     left: Matrix<T>,
     right: Matrix<T>,
 ) -> Matrix<T> {
     assert_eq!(left.cols, right.rows, "Invalid matrix dimensions");
-    let res = Arc::new(Mutex::new(Matrix::new(T::zero(), left.rows, right.cols)));
+    let mut res = Matrix::new(T::zero(), left.rows, right.cols);
     let left = Arc::new(left);
     let right = Arc::new(right);
 
-    let mut handles = Vec::new();
-    for row in 0..left.rows {
-        let res_clone = Arc::clone(&res);
-        let left_clone = Arc::clone(&left);
-        let right_clone = Arc::clone(&right);
+    let mut handles = Vec::with_capacity(left.rows);
+    for i in 0..left.rows {
+        let left = Arc::clone(&left);
+        let right = Arc::clone(&right);
         handles.push(thread::spawn(move || {
-            for iteration in 0..right_clone.cols {
+            let mut row_res = vec![T::zero(); right.cols];
+            for (j, val) in row_res.iter_mut().enumerate() {
                 let mut sum = T::zero();
-                for col in 0..left_clone.cols {
-                    sum += left_clone[row][col] * right_clone[col][iteration];
+                for k in 0..left.cols {
+                    sum += left[i][k] * right[k][j];
                 }
-                res_clone.lock().unwrap()[row][iteration] = sum;
+                *val = sum;
             }
+            row_res
         }));
     }
 
-    for handle in handles {
-        handle.join().unwrap();
+    for (i, handle) in handles.into_iter().enumerate() {
+        for (j, val) in handle.join().unwrap().iter().enumerate() {
+            res[i][j] = *val;
+        }
     }
 
-    Arc::into_inner(res).unwrap().into_inner().unwrap()
+    res
 }
--- parallel/tests/parallel.rs
+++ parallel/tests/parallel.rs
@@ -3,9 +3,17 @@ use parallel::*;
 
 #[test]
 fn test_multiply() {
+    let left = matrix![[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
+    let right = matrix![[13, 14, 15], [16, 17, 18], [19, 20, 21], [22, 23, 24]];
+    let expected = matrix![[190, 200, 210], [470, 496, 522], [750, 792, 834]];
+    assert_eq!(multiply(left, right), expected);
+}
+
+#[test]
+fn test_multiply_different_sizes() {
     let left = matrix![[1, 2, 3], [4, 5, 6]];
-    let right = matrix![[7, 8], [9, 10], [11, 12]];
-    let expected = matrix![[58, 64], [139, 154]];
+    let right = matrix![[7, 8, 9, 10], [11, 12, 13, 14], [15, 16, 17, 18]];
+    let expected = matrix![[74, 80, 86, 92], [173, 188, 203, 218]];
     assert_eq!(multiply(left, right), expected);
 }
 
