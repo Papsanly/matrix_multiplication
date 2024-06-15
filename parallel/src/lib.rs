55c759e7b562f53671d94bff6a145a4f256f1fcd
Papsanly <papsanlysenko@gmail.com>
2024-06-16 00:28:58 +0300

    minor optimization

--- parallel/src/lib.rs
+++ parallel/src/lib.rs
@@ -10,17 +10,20 @@ pub fn multiply<T: NumAssign + Clone + Copy + Send + Sync + Debug>(
     assert_eq!(left.cols, right.rows, "Invalid matrix dimensions");
     let mut res = Matrix::new(T::zero(), left.rows, right.cols);
 
-    let num_threads = num_threads.unwrap_or(
-        thread::available_parallelism()
-            .map(|v| v.get())
-            .unwrap_or(1),
-    );
+    let num_threads = num_threads
+        .unwrap_or(
+            thread::available_parallelism()
+                .map(|v| v.get())
+                .unwrap_or(1),
+        )
+        .min(res.rows);
+
     let iterations = res.cols;
     let (tx, rx) = mpsc::channel();
 
     let multiply_sub_matrix = |thread_idx| {
-        let from_row = thread_idx * res.rows / num_threads.min(res.rows);
-        let to_row = (thread_idx + 1) * res.rows / num_threads.min(res.rows);
+        let from_row = thread_idx * res.rows / num_threads;
+        let to_row = (thread_idx + 1) * res.rows / num_threads;
 
         for row in from_row..to_row {
             for iteration in 0..iterations {
@@ -34,7 +37,7 @@ pub fn multiply<T: NumAssign + Clone + Copy + Send + Sync + Debug>(
     };
 
     thread::scope(|s| {
-        for thread_idx in 0..num_threads.min(res.rows) {
+        for thread_idx in 0..num_threads {
             s.spawn(move || multiply_sub_matrix(thread_idx));
         }
     });
